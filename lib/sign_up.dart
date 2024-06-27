868f96a3a217b003a25c3aca1069354b48d1cd06
Ahmet Coko <ahmetcoko@gmail.com>
2024-06-27 14:45:38 +0300

    first commit

--- lib/sign_up.dart
+++ lib/sign_up.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:seat_ease/login_page.dart';
 import 'package:seat_ease/utils/customColors.dart';
 import 'package:seat_ease/utils/customTextStyle.dart';
-import 'home_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 
@@ -16,10 +15,13 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
-  late String email, fullname, username, password;
+  String email = '', fullname = '', username = '', password = '', confirmPassword = '';  // Changed to initial empty strings
   final formkey = GlobalKey<FormState>();
   final firebaseAuth = FirebaseAuth.instance;
   final authService = FirebaseAuth.instance;
+
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -54,6 +56,8 @@ class _SignUpState extends State<SignUp> {
                     customSizedBox(),
                     passwordTextField(),
                     customSizedBox(),
+                    confirmPasswordTextField(),  // Add this method call here
+                    customSizedBox(),
                     signUpButton(),
                     customSizedBox(),
                     backToLoginPage()
@@ -73,6 +77,18 @@ class _SignUpState extends State<SignUp> {
       style: CustomTextStyle.titleTextStyle,
     );
   }
+
+  TextFormField confirmPasswordTextField() {
+    return TextFormField(
+      controller: confirmPasswordController,
+      validator: (value) => confirmPasswordValidator(value, passwordController.text),
+      obscureText: true,
+      style: TextStyle(color: Colors.white),
+      decoration: customInputDecoration("Confirm Password"),
+      onSaved: (value) => confirmPassword = value!,  // Add this line
+    );
+  }
+
 
   TextFormField emailTextField() {
     return TextFormField(
@@ -119,19 +135,35 @@ class _SignUpState extends State<SignUp> {
     );
   }
 
+  @override
+  void initState() {
+    super.initState();
+    // Setup listeners to trigger form validation on change
+    passwordController.addListener(validatePassword);
+    confirmPasswordController.addListener(validatePassword);
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  void validatePassword() {
+    // Trigger validation logic for confirmPassword field
+    formkey.currentState?.validate();
+  }
+
   TextFormField passwordTextField() {
     return TextFormField(
-      validator: (value) {
-        if (value!.isEmpty) {
-          return "Bilgileri Eksiksiz Doldurunuz";
-        } else {}
-      },
-      onSaved: (value) {
-        password = value!;
-      },
+      controller: passwordController,
+      validator: passwordValidator,
       obscureText: true,
       style: TextStyle(color: Colors.white),
-      decoration: customInputDecoration("Sifre"),
+      decoration: customInputDecoration("Şifre"),
+      onSaved: (value) => password = value!,
     );
   }
 
@@ -148,42 +180,75 @@ class _SignUpState extends State<SignUp> {
   }
 
   void signUp() async {
-    if (formkey.currentState!.validate()) {
-      formkey.currentState!.save();
-      try {
-        // Create user with email and password
-        UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-          email: email,
-          password: password,
-        );
+    if (formkey.currentState!.validate()) { // Check if form inputs pass the validation rules
+      formkey.currentState!.save(); // Save the form data to the respective variables
 
-        // Save additional details in Firestore under the 'Users' collection
-        FirebaseFirestore.instance.collection('Users').doc(userCredential.user!.uid).set({
-          'fullname': fullname,
-          'username': username,
-          'email': email,
-          // Any other user details you might want to save
-        }).then((value) {
-          print("User Added to Firestore");
-          Navigator.of(context).pushAndRemoveUntil(
-            MaterialPageRoute(builder: (context) => LoginPage()),
-                (route) => false,
+      // Check if the passwords match, ensuring `password` and `confirmPassword` are not empty
+      if (password.isNotEmpty && confirmPassword.isNotEmpty && confirmPassword == password) {
+        try {
+          // Attempt to create a user with the provided email and password
+          UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+            email: email,
+            password: password,
           );
-        }).catchError((error) {
-          print("Failed to add user: $error");
-        });
 
-      } on FirebaseAuthException catch (e) {
-        if (e.code == 'weak-password') {
-          print('The password provided is too weak.');
-        } else if (e.code == 'email-already-in-use') {
-          print('The account already exists for that email.');
+          // Upon successful user creation, store additional user details in Firestore
+          FirebaseFirestore.instance.collection('Users').doc(userCredential.user!.uid).set({
+            'fullname': fullname,
+            'username': username,
+            'email': email,
+          }).then((value) {
+            // Navigate to the LoginPage after successful registration and data storage
+            Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(builder: (context) => LoginPage()),
+                  (route) => false,
+            );
+          }).catchError((error) {
+            // Handle errors related to Firestore operations
+            print("Failed to add user to Firestore: $error");
+            showErrorDialog("Error saving data", "Failed to save user details.");
+          });
+
+        } on FirebaseAuthException catch (e) {
+          // Handle errors related to Firebase authentication
+          if (e.code == 'weak-password') {
+            showErrorDialog("Weak Password", "The password provided is too weak.");
+          } else if (e.code == 'email-already-in-use') {
+            showErrorDialog("Email Already in Use", "The account already exists for that email.");
+          } else {
+            showErrorDialog("Authentication Error", e.message ?? "An unexpected error occurred.");
+          }
+        } catch (e) {
+          // Handle any other errors that might occur
+          showErrorDialog("Error", e.toString());
         }
-      } catch (e) {
-        print(e.toString());
+      } else {
+        // Handle the case where passwords do not match
+        showErrorDialog("Password Mismatch", "The passwords do not match. Please try again.");
       }
     }
   }
+
+  void showErrorDialog(String title, String message) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
+
 
 
   Center backToLoginPage() {
@@ -262,15 +327,18 @@ class _SignUpState extends State<SignUp> {
 
 // Password Validator
   String? passwordValidator(String? value) {
-    if (value!.isEmpty || value.length < 6) {
-      return "Password should be more than 6 characters.";
+    if (value == null || value.isEmpty) {
+      return 'Please enter a password.';
+    } else if (value.length < 6) {
+      return 'Password must be at least 6 characters long.';
     }
     return null;
   }
 
-// Confirm Password Validator
-  String? confirmPasswordValidator(String? value) {
-    if (value != password) {
+  String? confirmPasswordValidator(String? value, String password) {
+    if (value!.isEmpty) {
+      return "Please confirm your password.";
+    } else if (value != password) {
       return "Passwords do not match.";
     }
     return null;
