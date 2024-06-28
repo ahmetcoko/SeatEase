d51012e312fefcbd4c1981c1e2a1ec68d60fe0ee
Ahmet Coko <ahmetcoko@gmail.com>
2024-06-28 16:34:46 +0300

    first commit

--- lib/views/events_page.dart
+++ lib/views/events_page.dart
@@ -10,9 +10,8 @@ class EventsPage extends StatefulWidget {
 }
 
 class _EventsPageState extends State<EventsPage> {
-  Future<void> _refreshEvents() async {
-    setState(() {});
-  }
+  DateTime? _editingDate;
+  TimeOfDay? _editingTime;
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +30,7 @@ class _EventsPageState extends State<EventsPage> {
             return CircularProgressIndicator();
           }
           return RefreshIndicator(
-            onRefresh: _refreshEvents,
+            onRefresh: () => Future.sync(() => snapshot.requireData),
             child: ListView(
               children: snapshot.data!.docs.map((DocumentSnapshot document) {
                 Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
@@ -39,34 +38,62 @@ class _EventsPageState extends State<EventsPage> {
                 return Card(
                   child: ExpansionTile(
                     leading: Image.asset('assets/images/event.png', width: 40),
-                    title: Text(data['name'], style: TextStyle(fontWeight: FontWeight.bold)),
-                    subtitle: Text("Date-Time: ${DateFormat('yyyy-MM-dd – kk:mm').format(data['time'].toDate())} - ${data['participants'].length}/${data['capacity']}", style: TextStyle(fontWeight: FontWeight.normal)),
-                    trailing: Image.asset(isAvailable ? 'assets/images/available.png' : 'assets/images/cross.png', width: 24),
+                    title: Text(
+                      data['name'],
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                      "Date-Time: ${DateFormat('yyyy-MM-dd – kk:mm').format(data['time'].toDate())} - ${data['participants'].length}/${data['capacity']}",
+                      style: TextStyle(fontWeight: FontWeight.normal),
+                    ),
+                    trailing: Image.asset(
+                      isAvailable ? 'assets/images/available.png' : 'assets/images/cross.png',
+                      width: 24,
+                    ),
                     children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () => _changeDateTime(context, document.id),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context).primaryColor,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              minimumSize: Size(150, 50),
+                            ),
+                            child: Text(
+                              "Change Date-Time",
+                              style: TextStyle(color: Colors.pinkAccent),
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () => _deleteEvent(document.id),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.red,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              minimumSize: Size(150, 50),
+                            ),
+                            child: Text(
+                              "Delete Event",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
                       Padding(
                         padding: const EdgeInsets.all(16.0),
                         child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start, // Add this line
                           children: [
                             Text("Description", style: TextStyle(fontWeight: FontWeight.bold)),
                             Text(data['description']),
-                            SizedBox(height: 20),
+                            SizedBox(height: 10),
                             Text("Participants", style: TextStyle(fontWeight: FontWeight.bold)),
                             ...data['participants'].map<Widget>((name) => Text(name)).toList(),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                ElevatedButton(
-                                  onPressed: () => _selectDate(context, document.id),
-                                  child: Text("Change Date-Time"),
-                                ),
-                                ElevatedButton(
-                                  onPressed: () => _deleteEvent(document.id),
-                                  style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
-                                  child: Text("Delete Event"),
-                                ),
-                              ],
-                            )
                           ],
                         ),
                       ),
@@ -81,29 +108,44 @@ class _EventsPageState extends State<EventsPage> {
     );
   }
 
-  void _selectDate(BuildContext context, String docId) async {
+  void _changeDateTime(BuildContext context, String eventId) async {
     final DateTime? pickedDate = await showDatePicker(
       context: context,
-      initialDate: DateTime.now(),
+      initialDate: _editingDate ?? DateTime.now(),
       firstDate: DateTime(2000),
       lastDate: DateTime(2100),
     );
-    if (pickedDate != null) {
-      final TimeOfDay? pickedTime = await showTimePicker(
-        context: context,
-        initialTime: TimeOfDay.now(),
+    final TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: _editingTime ?? TimeOfDay.now(),
+    );
+
+    if (pickedDate != null && pickedTime != null) {
+      // Combine the date and time into one DateTime object
+      DateTime eventDateTime = DateTime(
+        pickedDate.year,
+        pickedDate.month,
+        pickedDate.day,
+        pickedTime.hour,
+        pickedTime.minute,
       );
-      if (pickedTime != null) {
-        DateTime fullDateTime = DateTime(pickedDate.year, pickedDate.month, pickedDate.day, pickedTime.hour, pickedTime.minute);
-        FirebaseFirestore.instance.collection('Events').doc(docId).update({
-          'time': fullDateTime,
-        });
-      }
+
+      // Update the event in Firestore
+      await FirebaseFirestore.instance.collection('Events').doc(eventId).update({
+        'time': eventDateTime,
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Event date-time updated successfully')),
+      );
     }
   }
 
-  void _deleteEvent(String docId) {
-    FirebaseFirestore.instance.collection('Events').doc(docId).delete();
+  void _deleteEvent(String eventId) async {
+    await FirebaseFirestore.instance.collection('Events').doc(eventId).delete();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Event deleted successfully')),
+    );
   }
 
 
