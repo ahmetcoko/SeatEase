454524eb1b4ba0423cb2716bc155fc95c9a617bd
Ahmet Coko <ahmetcoko@gmail.com>
2024-06-28 16:39:27 +0300

    first commit

--- lib/views/profile_page.dart
+++ lib/views/profile_page.dart
@@ -1,10 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'login_page.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Profile Page', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile"),
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.logout),
+          onPressed: () {
+            // Implement the logout functionality
+            _logout(context);
+          },
+        ),
+      ),
+      body: Center(
+        child: Text('Profile Page', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+      ),
     );
+  }
+
+  void _logout(BuildContext context) {
+    // Example logout functionality
+    // Assume you're using Firebase Authentication for this example
+    FirebaseAuth.instance.signOut().then((value) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => LoginPage()),
+      );
+    }).catchError((error) {
+      // Handle errors or notify user
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Logout failed: ${error.toString()}")),
+      );
+    });
   }
 }
