98e5ffc2da937a49337edb9e127af529e94f56c4
Ahmet Coko <ahmetcoko@gmail.com>
2024-07-02 16:43:13 +0300

    first commit

--- lib/views/user/user_events.dart
+++ lib/views/user/user_events.dart
@@ -133,81 +133,77 @@ class UserEventsPage extends StatefulWidget {
                   if (snapshot.connectionState == ConnectionState.waiting) {
                     return CircularProgressIndicator();
                   }
-                  return RefreshIndicator(
-                    onRefresh: () => Future.sync(() => snapshot.requireData),
-                    child: ListView(
-                      children: snapshot.data!.docs.map((DocumentSnapshot document) {
-                        Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-                        bool isUserJoined = data['participants'].any((participant) => participant['name'] == 'Ahmet Coko');  // Check if user is joined
-                        return InkWell(
-                          onTap: () {
-                            // Here you can use document.id which is the documentId of the clicked item
-                            print("Clicked event ID: ${document.id}");
-                            // Optionally, perform navigation or further actions with the documentId
-                          },
-                          child: Card(
-                            child: ExpansionTile(
-                              leading: Image.asset('assets/images/event.png', width: 40),
-                              title: Text(
-                                data['name'],
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                              subtitle: Text(
-                                "Date-Time: ${DateFormat('yyyy-MM-dd – kk:mm').format(data['time'].toDate())} - ${data['participants'].length}/${data['capacity']}",
-                                style: TextStyle(fontWeight: FontWeight.normal),
-                              ),
-                              trailing: Image.asset(
-                                isUserJoined
-                                    ? 'assets/images/res2.png'  // Show reserved image if user is joined
-                                    : data['participants'].length >= data['capacity']  // Check if event is full
-                                    ? 'assets/images/cross.png'  // Show cross image if event is full
-                                    : (data['time'].toDate().isBefore(DateTime.now())
-                                    ? 'assets/images/expired.png'  // Show expired image if the event date is past
-                                    : 'assets/images/available.png'),  // Otherwise, show available image
-                                width: 24,
-                              ),
-                              children: <Widget>[
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: _buildSeatGrid(
-                                      data['row'],
-                                      data['column'],
-                                      data['participants'],
-                                      document.id
-                                  ),
-                                ),
-                                Divider(),
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Container(
-                                        width: 20,
-                                        height: 20,
-                                        color: Colors.red.shade800,
-                                        margin: EdgeInsets.symmetric(horizontal: 10),
-                                      ),
-                                      Text("Full"),
-                                      Container(
-                                        width: 20,
-                                        height: 20,
-                                        color: Colors.green.shade200,
-                                        margin: EdgeInsets.symmetric(horizontal: 10),
-                                      ),
-                                      Text("Empty"),
-                                    ],
-                                  ),
-                                )
-                              ],
-                            ),
+                  return ListView(
+                    children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                      Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+                      return Card(
+                        child: ExpansionTile(
+                          leading: Image.asset('assets/images/event.png', width: 40),
+                          title: Text(
+                            data['name'],
+                            style: TextStyle(fontWeight: FontWeight.bold),
                           ),
-                        );
-                      }).toList(),
-                    ),
+                          subtitle: Text(
+                            "Date-Time: ${DateFormat('yyyy-MM-dd – kk:mm').format(data['time'].toDate())}",
+                            style: TextStyle(fontWeight: FontWeight.normal),
+                          ),
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "Description",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                data['description'] ?? 'No description provided',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(fontSize: 14),
+                              ),
+                            ),
+                            Divider(),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: _buildSeatGrid(
+                                  data['row'],
+                                  data['column'],
+                                  data['participants'],
+                                  document.id
+                              ),
+                            ),
+                            Divider(),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    width: 20,
+                                    height: 20,
+                                    color: Colors.red.shade800,
+                                    margin: EdgeInsets.symmetric(horizontal: 10),
+                                  ),
+                                  Text("Full"),
+                                  Container(
+                                    width: 20,
+                                    height: 20,
+                                    color: Colors.green.shade200,
+                                    margin: EdgeInsets.symmetric(horizontal: 10),
+                                  ),
+                                  Text("Empty"),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    }).toList(),
                   );
                 },
-              ),
+              )
             ),
           ],
         ),
